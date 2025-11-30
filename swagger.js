ec203c792bf22e53be8f671203d6a33967396381
Sergio Coria <sergiocoria.my@gmail.com>
2025-11-29 21:01:16 -0700

    Restore local swagger.js config and update swagger.json host to Render using HTTPS only

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'Team 16\'s API',
         description: 'This API was created by Team 16 to manage events.'
     },
-    host: isProd ? 'cse341-team16.onrender.com' : 'localhost:8080',
+    host: 'localhost:8080',
     schemes: ['http', 'https']
 };
 
